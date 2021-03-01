c38c5ad9b1ac107d1a02611209b9017ee4d1a472
FuryOFFools <AlderFrond@gmail.com>
2021-03-02 01:30:42 +0400

    small fix 3

--- lib/Classes/fetch_album.dart
+++ lib/Classes/fetch_album.dart
@@ -2,10 +2,9 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:weather/Classes/album.dart';
+import 'package:weather/Classes/album5days.dart';
 
 Future<Album> fetchAlbumWithCoords(String api, String lat, String lon) async {
-  //final response = await http.get(Uri.https('api.openweathermap.org',
-  //    '/data/2.5/weather', {'q': '$location', 'appid': '$api'}));
   //TODO: сделать империческую (фаренгейт)
   final response = await http.get(Uri.https(
       'api.openweathermap.org',
@@ -27,6 +26,33 @@ Future<Album> fetchAlbumWithCity(String api, String location) async {
 
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<Album> fetchAlbum5WithCoords(String api, String lat, String lon) async {
+  final response = await http.get(Uri.https(
+      'api.openweathermap.org',
+      '/data/2.5/forecast',
+      {'lat': lat, 'lon': lon, 'appid': '$api', 'units': 'metric'}));
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<Album5Days> fetchAlbum5WithCity(String api, String location) async {
+  //TODO: сделать обработку
+  final response = await http.get(Uri.https(
+      'api.openweathermap.org',
+      '/data/2.5/forecast',
+      {'q': '$location', 'appid': '$api', 'units': 'metric'}));
+
+  if (response.statusCode == 200) {
+    return Album5Days.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load album');
   }
