b434ec436f901e90bb7bf12189c0d989067cf8d2
FuryOFFools <AlderFrond@gmail.com>
2021-03-08 23:36:08 +0400

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:weather/LoadingPage/loading_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoadingPage(),
+      home: Text('new branch'),
     );
   }
 }
